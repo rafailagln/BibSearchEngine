e9e840456af5cf7e5597d8164d5496c6f1b0070e
Rafaila Galanopoulou <rafgalan98@gmail.com>
2023-03-14 12:42:02 +0200

    Add connection class

--- src/back/Indexer/main.py
+++ src/back/Indexer/main.py
@@ -1,20 +1,12 @@
 from Preprocessor.DataCleaner import DataCleaner
-import DataReader
+from Basics.connection import MongoDBConnection
 from Indexer.IndexValueInfo import InfoClass
 import pprint
 
-mongoConnection = DataReader.Reader('mongodb://m151User:YyKOhV1xa3mnmlFP@vmi1224404.contaboserver.net:27017/?authMechanism=DEFAULT&authSource=M151')
-collection = mongoConnection.getCollection("M151", "Papers")
-results = mongoConnection.readCollection(collection)
-
-# presults = list(results)
-# for result in presults:
-#     print(result)
-
 index_dictionary = dict()
 cleaner = DataCleaner()
-
-
+#
+#
 def nodeAdder(_id, _value, _counter, _index_dictionary, type):
     cleaned_words = cleaner.cleanData(_value)
     for word in cleaned_words:
@@ -26,21 +18,21 @@ def nodeAdder(_id, _value, _counter, _index_dictionary, type):
         _counter += 1
     return _counter, _index_dictionary
 
-
-for doc in results:
-    doc_id = doc["_id"]
-    for field, value in doc.items():
-        counter = 1
-        if field == "title":
-            counter, index_dictionary = nodeAdder(doc_id, value[0], counter, index_dictionary, "title")
-        elif field == "abstract":
-            counter, index_dictionary = nodeAdder(doc_id, value, counter, index_dictionary, "abstract")
+with MongoDBConnection() as collection:
+    data = collection.find({}, {"_id": 1, "title": 1, "abstract": 1})
+    for doc in data:
+        doc_id = doc["_id"]
+        for field, value in doc.items():
+            counter = 1
+            if field == "title":
+                counter, index_dictionary = nodeAdder(doc_id, value[0], counter, index_dictionary, "title")
+            elif field == "abstract":
+                counter, index_dictionary = nodeAdder(doc_id, value, counter, index_dictionary, "abstract")
 
 # for i in index_dictionary.values():
 #     pprint.pprint(i[0])
 # pprint.pprint(index_dictionary)
 # for key, value in index_dictionary.items():
 #     print("key: \"", key, "\", value: \"", value[0]._type, "\"")
-#
-# print()
-# print(len(index_dictionary))
+
+
