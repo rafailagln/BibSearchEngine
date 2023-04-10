fe35ed81c14e98288c92962a0c164c10f6bb46ec
Notaris <80954329+notarisj@users.noreply.github.com>
2023-04-10 19:06:50 +0300

    Fixed endpoint in fetchDataForIds in Deploy

--- src/front/src/main/resources/static/js/pagination.js
+++ src/front/src/main/resources/static/js/pagination.js
@@ -139,7 +139,7 @@ function createNextButton() {
 
 async function fetchDataForIds(ids) {
     // Make the API call to fetch data for the given document IDs
-    const response = await fetch('http://127.0.0.1:5000/fetch_data/', {
+    const response = await fetch('http://vmi1224404.contaboserver.net:5000/fetch_data/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
