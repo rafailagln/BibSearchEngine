e8f3da9a2014fd8b6e2b6e33661a998ab7d2060d
Notaris <80954329+notarisj@users.noreply.github.com>
2023-04-28 18:49:11 +0300

    Minor factorisation

---
+++ src/back/distributed/test_get_data.py
@@ -0,0 +1,21 @@
+import json
+import time
+
+from distributed.utils import send_request
+
+
+def main():
+    start_time = time.time()
+    node1_addr = ('localhost', 8083)
+    # Insert key-value pairs
+    insert_request = \
+        {'action': 'get_data', 'forwarded': False, 'ids': list(range(1, 1000))}
+    response = send_request(node1_addr, insert_request)
+    print(f"Insert response: {response['results']}")
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print("Execution time:", execution_time, "seconds")
+
+
+if __name__ == '__main__':
+    main()
