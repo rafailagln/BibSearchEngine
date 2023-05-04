f4b95ae35958dbf69a8742bc9d2f5e0af36fa39a
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-05-04 15:32:30 +0300

    Revert "New thread for search_ids and fetch_data in FastAPI"
    This reverts commit 370208a2a4950cb5a8e4e9828acc4de581cc001f.

--- src/back/FastAPI.py
+++ src/back/FastAPI.py
@@ -1,7 +1,7 @@
 import logging
 import time
+
 import uvicorn
-import concurrent.futures
 import json
 
 from fastapi import FastAPI
@@ -9,6 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 from distributed.request_wrapper import search_ids_wrapper, fetch_data_wrapper
 
+# TODO: add search_ids and fetch_data in new thread to have non-blocking actions
 # TODO: convert FastAPI to accept request from frond, forward to node, get results
 #       and then fetch results in buckets
 
@@ -32,12 +33,13 @@ app.add_middleware(
 )
 
 
+# TODO: make wrapper function to not call engine and send-receive results like the function
+#  "def send_request_wrapper(node)" in "def execute_action()"
+
 @app.get('/search_ids/{query}', response_model=List[int])
 def search_ids(query: str):
     start_time = time.time()
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future = executor.submit(search_ids_wrapper, query)
-    ids = future.result()
+    ids = search_ids_wrapper(query)
     logging.info(f"Returned {len(ids)} document IDs")
     end_time = time.time()
     logging.info(f"Elapsed time: {end_time - start_time}")
@@ -46,9 +48,7 @@ def search_ids(query: str):
 
 @app.post('/fetch_data/', response_model=List[dict])
 def fetch_data(ids: List[int]):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future = executor.submit(fetch_data_wrapper, ids)
-    results = future.result()
+    results = fetch_data_wrapper(ids)
     logging.info(f"Fetched data for {len(results['results'])} documents")
     return results['results']
 
