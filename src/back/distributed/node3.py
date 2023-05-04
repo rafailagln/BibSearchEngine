20542c17d86e54a8e836919936300d49a0519b5e
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-05-04 13:45:15 +0300

    Added new thread for every connection

--- src/back/distributed/node3.py
+++ src/back/distributed/node3.py
@@ -15,7 +15,6 @@ from distributed.fast_json_loader import FastJsonLoader
 from distributed.utils import send_request, receive_message, send_message
 
 # TODO: Check all blocking parts (see example execute_action with new thread)
-# TODO: Send request in chunks like SOCKETS/SOCKETS2
 
 
 def process_data(data):
@@ -161,13 +160,14 @@ class DistributedNode:
             while True:
                 conn, addr = sock.accept()
                 print(f"Node {self.node_id}: Connection accepted from {addr}")
-                # request = conn.recv(10000).decode()     # receive in chunks
-                request = receive_message(conn)
-                print(f"Node {self.node_id}: Received request: {request[:100]}")
-                response = self.handle_request(request)     # --> make a new thread to run handle_request
-                print(f"Node {self.node_id}: Sending response: {response[:100]}")
-                # conn.sendall(response.encode())     # send in chunks # new thread here for sending request
-                send_message(response, conn)
+                threading.Thread(target=self.handle_request_wrapper, args=(conn,)).start()
+
+    def handle_request_wrapper(self, conn):
+        request = receive_message(conn)
+        print(f"Node {self.node_id}: Received request: {request[:100]}")
+        response = self.handle_request(request)
+        print(f"Node {self.node_id}: Sending response: {response[:100]}")
+        send_message(response, conn)
 
     # run with encryption
     # def run(self):
