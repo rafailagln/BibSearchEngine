8a986438df3cf295b1b6e2f85be21044c8fd411f
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-05-04 15:37:29 +0300

    New thread for search_ids and fetch_data in FastAPI
    search_ids and fetch_data now run in a new thread

--- src/back/FastAPI.py
+++ src/back/FastAPI.py
@@ -1,7 +1,7 @@
 import logging
 import time
-
 import uvicorn
+import concurrent.futures
 import json
 
 from fastapi import FastAPI
@@ -33,13 +33,12 @@ app.add_middleware(
 )
 
 
-# TODO: make wrapper function to not call engine and send-receive results like the function
-#  "def send_request_wrapper(node)" in "def execute_action()"
-
 @app.get('/search_ids/{query}', response_model=List[int])
 def search_ids(query: str):
     start_time = time.time()
-    ids = search_ids_wrapper(query)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(search_ids_wrapper, query)
+    ids = future.result()
     logging.info(f"Returned {len(ids)} document IDs")
     end_time = time.time()
     logging.info(f"Elapsed time: {end_time - start_time}")
@@ -48,7 +47,9 @@ def search_ids(query: str):
 
 @app.post('/fetch_data/', response_model=List[dict])
 def fetch_data(ids: List[int]):
-    results = fetch_data_wrapper(ids)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(fetch_data_wrapper, ids)
+    results = future.result()
     logging.info(f"Fetched data for {len(results['results'])} documents")
     return results['results']
 
