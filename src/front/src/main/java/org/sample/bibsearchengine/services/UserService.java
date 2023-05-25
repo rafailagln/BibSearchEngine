6fc763c686958e2165bdd39151cd5739d3fac702
Notaris <80954329+notarisj@users.noreply.github.com>
2023-05-26 02:39:09 +0300

    Remove postgress database from front

--- src/front/src/main/java/org/sample/bibsearchengine/services/UserService.java
+++ src/front/src/main/java/org/sample/bibsearchengine/services/UserService.java
@@ -1,25 +1,21 @@
-package org.sample.bibsearchengine.services;
-
-import org.sample.bibsearchengine.entities.AppUser;
-import org.sample.bibsearchengine.repositories.UserRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-
-    private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-//    public void saveUser(AppUser appUser) {
-//        userRepository.save(appUser);
+//package org.sample.bibsearchengine.services;
+//
+//import org.sample.bibsearchengine.entities.AppUser;
+//import org.sample.bibsearchengine.repositories.UserRepository;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class UserService {
+//
+//    private final UserRepository userRepository;
+//
+//    public UserService(UserRepository userRepository) {
+//        this.userRepository = userRepository;
 //    }
-
-    public boolean checkUserCredentials(String username, String password) {
-        AppUser appUser = userRepository.findByUsername(username);
-        return appUser != null && appUser.getPassword().equals(password);
-    }
-}
-
+//
+//    public boolean checkUserCredentials(String username, String password) {
+//        AppUser appUser = userRepository.findByUsername(username);
+//        return appUser != null && appUser.getPassword().equals(password);
+//    }
+//}
+//
