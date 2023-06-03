42f9e6ad7f6e07fe9b8082fb6f6bcbd9cb02351a
Notaris <80954329+notarisj@users.noreply.github.com>
2023-06-03 18:00:07 +0300

    Add python api call url from properties file

--- src/front/src/main/java/org/sample/bibsearchengine/controllers/ProxyController.java
+++ src/front/src/main/java/org/sample/bibsearchengine/controllers/ProxyController.java
@@ -1,6 +1,8 @@
 package org.sample.bibsearchengine.controllers;
 
 import java.util.List;
+import java.io.IOException;
+import java.util.Properties;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -17,6 +19,19 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/api")
 public class ProxyController {
 
+    private static final String EXTERNAL_API_URL;
+
+    static {
+        // load the properties file
+        Properties properties = new Properties();
+        try {
+            properties.load(ProxyController.class.getClassLoader().getResourceAsStream("application.properties"));
+            EXTERNAL_API_URL = properties.getProperty("external.api.call");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @PostMapping("/fetch_data/")
     public ResponseEntity<?> fetchDataForIds(@RequestBody List<String> ids) {
         return callExternalApi(ids);
@@ -24,12 +39,11 @@ public class ProxyController {
 
     private ResponseEntity<String> callExternalApi(List<String> ids) {
         RestTemplate restTemplate = new RestTemplate();
-        String externalApiUrl = "http://127.0.0.1:5000/fetch_data/";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<List<String>> requestEntity = new HttpEntity<>(ids, headers);
-        return restTemplate.exchange(externalApiUrl, HttpMethod.POST, requestEntity, String.class);
+        return restTemplate.exchange(EXTERNAL_API_URL, HttpMethod.POST, requestEntity, String.class);
     }
 }
