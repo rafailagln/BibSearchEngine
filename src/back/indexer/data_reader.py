836e8949613656001d22dd996627d6449868559f
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-06-03 17:57:59 +0300

    Add comments to the rest methods

--- src/back/indexer/data_reader.py
+++ src/back/indexer/data_reader.py
@@ -8,6 +8,16 @@ class Reader:
         self._connectionString = connection_string
 
     def get_collection(self, db_name, collection_name):
+        """
+        Retrieves a collection from the MongoDB database.
+
+        Inputs:
+        - db_name: The name of the database.
+        - collection_name: The name of the collection.
+
+        Output:
+        - The requested collection object.
+        """
         client = pymongo.MongoClient(self._connectionString)
         db = client[db_name]
         collection = db[collection_name]
@@ -15,8 +25,27 @@ class Reader:
 
     @staticmethod
     def read_collection(collection):
+        """
+        Reads all documents from the provided collection.
+
+        Input:
+        - collection: The collection to read documents from.
+
+        Output:
+        - A cursor object representing the result of the query.
+        """
         return collection.find({}, {"_id": 1, "title": 1, "abstract": 1})
 
     @staticmethod
     def read_limited_collection(collection, limit):
+        """
+        Reads a limited number of documents from the provided collection.
+
+        Inputs:
+        - collection: The collection to read documents from.
+        - limit: The maximum number of documents to retrieve.
+
+        Output:
+        - A cursor object representing the result of the query.
+        """
         return collection.find({}, {"_id": 1, "title": 1, "abstract": 1}).limit(limit)
--- src/back/indexer/index_valueInfo.py
+++ src/back/indexer/index_valueInfo.py
@@ -1,12 +1,30 @@
 class InfoClass:
 
     def __init__(self, type, position, docid) -> None:
+        """
+        Initializes an instance of the InfoClass.
+
+        Inputs:
+        - type: The type of the information.
+        - position: The position of the information.
+        - docid: The document ID associated with the information.
+
+        Outputs: None
+        """
         super().__init__()
         self._type = type
         self._position = position
         self._docid = docid
 
     def __json__(self):
+        """
+        Returns a dictionary representation of the InfoClass instance.
+
+        Input: None
+
+        Output:
+        - A dictionary representing the InfoClass instance.
+        """
         return {
             'type': self._type,
             'position': self._position,
